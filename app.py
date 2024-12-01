7c77dd613dde52ff1bcbd6e091a432fe0ff83004
Kenny Igbechi <kennyigbechi@microsoft.com>
2024-12-01 10:19:33 -0800

    added homepage

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import openai
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route("/")
+def home():
+    return render_template("index.html", user_name="Guest")
+
 
 if __name__ == '__main__':
     app.run()
